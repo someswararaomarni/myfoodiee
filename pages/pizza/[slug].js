c3d527d85d44671df95176b8ab89bfbfe61d5aa6
someswararaomarni <someswararaomarni@gmail.com>
2022-08-19 09:06:25 +0530

    Completed

--- pages/pizza/[slug].js
+++ pages/pizza/[slug].js
@@ -7,8 +7,7 @@ import css from '../../styles/Pizza.module.css';
 import { useState } from 'react';
 import { useStore } from '../../store/store';
 import toast, {Toaster} from 'react-hot-toast';
-export default function Pizza() {
-    const pizza = {pizza};
+export default function Pizza({pizza}) {
     const src=urlFor(pizza.image).url();
     const [Size,setSize]=useState(1);
     const [Quantity,setQuantity]=useState(1);
